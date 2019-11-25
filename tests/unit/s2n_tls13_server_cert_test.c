5386ae75826fea7e91c73db1f917ed66557f5554
Ravi Singh <ravsi@amazon.com>
2019-11-25 15:33:45 -0800

    adding support for server certificate request context (#1201)

--- tests/unit/s2n_tls13_server_cert_test.c
+++ tests/unit/s2n_tls13_server_cert_test.c
@@ -23,6 +23,7 @@
 #include "testlib/s2n_testlib.h"
 #include "tls/s2n_tls.h"
 #include "utils/s2n_safety.h"
+#include "crypto/s2n_certificate.h"
 
 /* Test vectors from https://tools.ietf.org/html/rfc8448#section-3 */
 const char tls13_cert_hex[] =
@@ -48,6 +49,33 @@ const char tls13_cert_hex[] =
     "c1fc63a42a99be5c3eb7107c3c54e9b9eb2bd5203b"
     "1c3b84e0a8b2f759409ba3eac9d91d402dcc0cc8f8"
     "961229ac9187b42b4de10000";
+
+const char tls13_cert_chain_hex[] =
+    "0001b03082" /* without chertificate size */
+    "01ac30820115a003020102020102300d06092a8648"
+    "86f70d01010b0500300e310c300a06035504031303"
+    "727361301e170d3136303733303031323335395a17"
+    "0d3236303733303031323335395a300e310c300a06"
+    "03550403130372736130819f300d06092a864886f7"
+    "0d010101050003818d0030818902818100b4bb498f"
+    "8279303d980836399b36c6988c0c68de55e1bdb826"
+    "d3901a2461eafd2de49a91d015abbc9a95137ace6c"
+    "1af19eaa6af98c7ced43120998e187a80ee0ccb052"
+    "4b1b018c3e0b63264d449a6d38e22a5fda43084674"
+    "8030530ef0461c8ca9d9efbfae8ea6d1d03e2bd193"
+    "eff0ab9a8002c47428a6d35a8d88d79f7f1e3f0203"
+    "010001a31a301830090603551d1304023000300b06"
+    "03551d0f0404030205a0300d06092a864886f70d01"
+    "010b05000381810085aad2a0e5b9276b908c65f73a"
+    "7267170618a54c5f8a7b337d2df7a594365417f2ea"
+    "e8f8a58c8f8172f9319cf36b7fd6c55b80f21a0301"
+    "5156726096fd335e5e67f2dbf102702e608ccae6be"
+    "c1fc63a42a99be5c3eb7107c3c54e9b9eb2bd5203b"
+    "1c3b84e0a8b2f759409ba3eac9d91d402dcc0cc8f8"
+    "961229ac9187b42b4de10000";
+
+const char tls13_cert_chain_size_hex[] =
+     "0001b03082"; /* certificate size */
 
 int main(int argc, char **argv)
 {
@@ -75,6 +103,28 @@ int main(int argc, char **argv)
         EXPECT_SUCCESS(s2n_stuffer_write(&conn->handshake.io, &tls13_cert));
         EXPECT_FAILURE(s2n_server_cert_recv(conn));
 
+        EXPECT_SUCCESS(s2n_connection_free(conn));
+    }
+
+    /* Test s2n_server_cert_send() verify certificate request context */
+    {
+        S2N_BLOB_FROM_HEX(tls13_cert_chain, tls13_cert_chain_hex);
+        S2N_BLOB_FROM_HEX(tls13_cert_chain_size, tls13_cert_chain_size_hex);
+
+        struct s2n_connection *conn;
+        EXPECT_NOT_NULL(conn = s2n_connection_new(S2N_SERVER));
+
+        struct s2n_cert cert = {.raw = tls13_cert_chain, .next= NULL};
+        struct s2n_cert_chain cert_chain = {.head = &cert};
+        conn->actual_protocol_version = S2N_TLS13;
+        struct s2n_cert_chain_and_key cert_chain_and_key = {.cert_chain = &cert_chain};
+        conn->handshake_params.our_chain_and_key = &cert_chain_and_key;
+
+        EXPECT_SUCCESS(s2n_server_cert_send(conn));
+        uint8_t certificate_request_context;
+        GUARD(s2n_stuffer_read_uint8(&conn->handshake.io, &certificate_request_context));
+        /* certificate request context for server should always be zero */
+        EXPECT_EQUAL(certificate_request_context, 0);
         EXPECT_SUCCESS(s2n_connection_free(conn));
     }
 
